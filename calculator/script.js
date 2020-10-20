db83139b74b7855813b186564ad0cb7384bb646d
eriksol <erik.solg@tallink.ee>
2020-10-20 21:28:13 +0300

    Rewrite JS

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,41 @@
+const ADD = 1;
+const SUBTRACT = 2;
+const MULTIPLY = 3;
+const DIVIDE = 4;
+
 const calculatorText = document.querySelector('#calculator-text');
-const operationButtons = document.querySelectorAll('.operation');
-const equals = document.querySelector('#equals');
-const clear = document.querySelector('#clear');
+
+const numbers = [
+    document.querySelector('#number-1'),
+    document.querySelector('#number-2'),
+    document.querySelector('#number-3'),
+    document.querySelector('#number-4'),
+    document.querySelector('#number-5'),
+    document.querySelector('#number-6'),
+    document.querySelector('#number-7'),
+    document.querySelector('#number-8'),
+    document.querySelector('#number-9'),
+    document.querySelector('#number-0')
+]
+
+/** Operators */
+const operators = [
+    ADD => document.querySelector('#operator-add'),
+    SUBTRACT => document.querySelector('#operator-subtract'),
+    MULTIPLY => operatorMultiply = document.querySelector('#operator-multiply'),
+    DIVIDE => document.querySelector('#operator-divide')
+]
+
+/** Actions */
+const actionEquals = document.querySelector('#action-equals');
+const actionClear = document.querySelector('#action-clear');
+
 const maxCalculationSize = 14;
+
+let lastOperator = ADD;
+let lastResult = 0;
+let currentNumber = '';
+let currentOperator = null;
 
 function add(a, b) {
     return a + b;
@@ -22,136 +55,34 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     switch (operator) {
-        case '+':
+        case ADD:
             return add(a, b);
-        case '-':
+        case SUBTRACT:
             return subtract(a, b);
-        case '*':
+        case MULTIPLY:
             return multiply(a, b);
-        case '/':
+        case DIVIDE:
             return divide(a, b);
     }
 }
 
-function evaluateAcceptableInput(input) {
-    if (currentCalcHasOperator() && isOperator(input)) {
-        return false;
-    }
+function toggleCurrentOperator() {
 
-    if (currentCalculation.length == 0 && isOperator(input)) {
-        return false;
-    }
-
-    if (maxCalculationSize - currentCalculation.length == 0) {
-        return false;
-    }
-
-    if (!currentCalcHasOperator() && (maxCalculationSize - currentCalculation.length == 2) && !isOperator(input)) {
-        return false;
-    }
-
-    return true;
 }
 
-function isOperator(input) {
-    return input == '+' ||
-        input == '-' ||
-        input == '*' ||
-        input == '/';
-}
+function handleNumberInput(number) {
+    if (currentOperator) {
+        newResult = operate(currentOperator, lastResult, currentNumber);
+        lastResult = newResult;
+        toggleCurrentOperator();
 
-function currentCalcHasOperator() {
-    return currentCalculation.reduce(((hasOperator, value) => hasOperator || isOperator(value)), false);
-}
-
-function updateCurrentCalculation(input) {
-    if (evaluateAcceptableInput(input)) {
-        currentCalculation.push(input);
-        updateCalculatorText();
+    } else if (maxCalculationSize - currentNumber.length) {
+        currentNumber += number;
+        calculatorText.textContent = currentNumber;
     }
 }
 
-function clearCurrentCalculation() {
-    currentCalculation = [];
-    calculatorText.textContent = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => handleNumberInput(i + 1));
 }
-
-function parseCurrentCalculation() {
-    if (!currentCalcHasOperator()) {
-        return;
-    }
-    let operatorIndex = currentCalculation.findIndex(element => isOperator(element));
-    let operator = currentCalculation[operatorIndex];
-    let operand1 = +currentCalculation.slice(0, operatorIndex).join('');
-    let operand2 = +currentCalculation.slice(operatorIndex + 1).join('');
-    let result = operate(operator, operand1, operand2);
-    if (result.toString().length > maxCalculationSize) {
-        calculatorText.textContent = 1;
-    } else {
-        calculatorText.textContent = result;
-    }
-    currentCalculation = [];
-}
-
-function updateCalculatorText() {
-    let calculationString = currentCalculation.join('');
-    calculatorText.textContent = calculationString;
-}
-
-function operationButtonPressed(event) {
-    switch (event.target.id) {
-        case 'number-1':
-            updateCurrentCalculation(1);
-            break;
-        case 'number-2':
-            updateCurrentCalculation(2);
-            break;
-        case 'number-3':
-            updateCurrentCalculation(3);
-            break;
-        case 'number-4':
-            updateCurrentCalculation(4);
-            break;
-        case 'number-5':
-            updateCurrentCalculation(5);
-            break;
-        case 'number-6':
-            updateCurrentCalculation(6);
-            break;
-        case 'number-7':
-            updateCurrentCalculation(7);
-            break;
-        case 'number-8':
-            updateCurrentCalculation(8);
-            break;
-        case 'number-9':
-            updateCurrentCalculation(9);
-            break;
-        case 'number-0':
-            updateCurrentCalculation(0);
-            break;
-        case 'add':
-            updateCurrentCalculation('+');
-            break;
-        case 'subtract':
-            updateCurrentCalculation('-');
-            break;
-        case 'multiply':
-            updateCurrentCalculation('*');
-            break;
-        case 'divide':
-            updateCurrentCalculation('/');
-            break;
-    }
-}
-
-let currentCalculation = [];
-
-for (let i = 0; i < operationButtons.length; i++) {
-    operationButtons[i].addEventListener('click', operationButtonPressed);
-}
-
-equals.addEventListener('click', parseCurrentCalculation);
-clear.addEventListener('click', clearCurrentCalculation);
-
-clearCurrentCalculation();
