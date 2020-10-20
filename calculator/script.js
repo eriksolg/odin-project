367239766fd5edf11a48549db736572ce3858e23
eriksol <erik.solg@tallink.ee>
2020-10-20 23:16:10 +0300

    Made functional with new logic

--- calculator/script.js
+++ calculator/script.js
@@ -2,10 +2,13 @@ const ADD = 1;
 const SUBTRACT = 2;
 const MULTIPLY = 3;
 const DIVIDE = 4;
+const maxCalculationSize = 14;
 
 const calculatorText = document.querySelector('#calculator-text');
 
+
 const numbers = [
+    document.querySelector('#number-0'),
     document.querySelector('#number-1'),
     document.querySelector('#number-2'),
     document.querySelector('#number-3'),
@@ -14,28 +17,37 @@ const numbers = [
     document.querySelector('#number-6'),
     document.querySelector('#number-7'),
     document.querySelector('#number-8'),
-    document.querySelector('#number-9'),
-    document.querySelector('#number-0')
+    document.querySelector('#number-9')
 ]
 
-/** Operators */
-const operators = [
-    ADD => document.querySelector('#operator-add'),
-    SUBTRACT => document.querySelector('#operator-subtract'),
-    MULTIPLY => operatorMultiply = document.querySelector('#operator-multiply'),
-    DIVIDE => document.querySelector('#operator-divide')
-]
+const operators = new Map([
+    [ADD, document.querySelector('#operator-add')],
+    [SUBTRACT, document.querySelector('#operator-subtract')],
+    [MULTIPLY, document.querySelector('#operator-multiply')],
+    [DIVIDE, document.querySelector('#operator-divide')]
+]);
 
-/** Actions */
-const actionEquals = document.querySelector('#action-equals');
-const actionClear = document.querySelector('#action-clear');
+for (let [operator, element] of operators) {
+    element.addEventListener('click', () => setCurrentOperator(operator));
+}
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => handleNumberInput(i));
+}
+document.querySelector('#action-equals').addEventListener('click', () => handleFinalResult());
+document.querySelector('#action-clear').addEventListener('click', () => resetState(true));
 
-const maxCalculationSize = 14;
+let lastOperator;
+let lastResult;
+let currentNumber;
+let currentOperator;
 
-let lastOperator = ADD;
-let lastResult = 0;
-let currentNumber = '';
-let currentOperator = null;
+function resetState(resetScreen) {
+    currentNumber = '';
+    lastResult = 0;
+    lastOperator = ADD;
+    resetCurrentOperator();
+    resetScreen ? calculatorText.textContent = 0 : null;
+}
 
 function add(a, b) {
     return a + b;
@@ -66,23 +78,51 @@ function operate(operator, a, b) {
     }
 }
 
-function toggleCurrentOperator() {
+function resetCurrentOperator() {
+    if (currentOperator) {
+        operators.get(currentOperator).classList.remove('active');
+    }
+    currentOperator = null;
+}
 
+function setCurrentOperator(operator) {
+    if (currentOperator == operator) {
+        resetCurrentOperator();
+        return
+    }
+    if (currentOperator) {
+        resetCurrentOperator();
+    }
+    operators.get(operator).classList.add('active');
+    currentOperator = operator;
+}
+
+function updateCalculatorText() {
+    calculatorText.textContent = currentNumber;
 }
 
 function handleNumberInput(number) {
     if (currentOperator) {
-        newResult = operate(currentOperator, lastResult, currentNumber);
+        newResult = operate(lastOperator, +lastResult, +currentNumber);
         lastResult = newResult;
-        toggleCurrentOperator();
-
+        lastOperator = currentOperator;
+        resetCurrentOperator();
+        currentNumber = '';
+        currentNumber += number;
+        updateCalculatorText();
     } else if (maxCalculationSize - currentNumber.length) {
         currentNumber += number;
-        calculatorText.textContent = currentNumber;
+        updateCalculatorText();
     }
 }
 
-
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', () => handleNumberInput(i + 1));
+function handleFinalResult() {
+    if (currentOperator) {
+        return;
+    }
+    result = operate(lastOperator, +lastResult, +currentNumber);
+    calculatorText.textContent = result
+    resetState(false);
 }
+
+resetState(true);
