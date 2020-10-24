9d4c4a89f8c6fe948c1474f5036c0de0da2b62ed
eriksol <erik.solg@tallink.ee>
2020-10-24 16:06:21 +0300

    Adding and listing books functionality

--- library/script.js
+++ library/script.js
@@ -1,4 +1,11 @@
 let myLibrary = [];
+const bookForm = document.querySelector('#book-form');
+const bookTitleInput = document.querySelector('#book-title-input');
+const bookAuthorInput = document.querySelector('#book-author-input');
+const bookPagesInput = document.querySelector('#book-pages-input');
+
+const existingBooks = document.querySelector('#existing-books');
+const bookTemplate = document.querySelector('#book-template');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,12 +17,37 @@ function Book(title, author, pages, read) {
     }
 }
 
-function getUserInput() {}
+function submitForm() {
+    let title = bookTitleInput.value;
+    let author = bookAuthorInput.value;
+    let pages = bookPagesInput.value;
+    myLibrary.push(new Book(title, author, pages, false));
+    populateLibrary();
+}
 
-function addBookToLibrary(title, author, pages, read) {}
+function getBookTemplate() {
+    let template = document.querySelector('#book-template');
+    return document.importNode(template.content, true);
+}
+
+function addBookHTML(book, index) {
+    let newBookHTML = bookTemplate.content.cloneNode(true);
+
+    newBookHTML.querySelector('.book-number').textContent = `Book ${index + 1}`;
+    newBookHTML.querySelector('.book-title').textContent = book.title;
+    newBookHTML.querySelector('.book-author').textContent = book.author;
+    newBookHTML.querySelector('.book-pages').textContent = book.pages;
+
+    existingBooks.appendChild(newBookHTML);
+}
+
+bookForm.addEventListener('submit', submitForm);
 
 function removeBookFromLibrary(book) {}
 
 function toggleBookReadStatus(book) {}
 
-function populateBookTable() {}
+function populateLibrary() {
+    existingBooks.innerHTML = '';
+    myLibrary.forEach(addBookHTML);
+}
