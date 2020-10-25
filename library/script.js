8a04faa149cfcf8ef082295fbeb8f258e64280d8
eriksol <erik.solg@tallink.ee>
2020-10-25 22:11:30 +0200

    Finish library

--- library/script.js
+++ library/script.js
@@ -1,11 +1,21 @@
 let myLibrary = [];
-const bookForm = document.querySelector('#book-form');
+if (localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+}
+
+
+const submitBook = document.querySelector('#submit-book');
 const bookTitleInput = document.querySelector('#book-title-input');
 const bookAuthorInput = document.querySelector('#book-author-input');
 const bookPagesInput = document.querySelector('#book-pages-input');
-
 const existingBooks = document.querySelector('#existing-books');
 const bookTemplate = document.querySelector('#book-template');
+
+submitBook.addEventListener('click', submitForm);
+
+function updateLocalStorage() {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -17,11 +27,15 @@ function Book(title, author, pages, read) {
     }
 }
 
-function submitForm() {
+function submitForm(event) {
+    event.preventDefault();
     let title = bookTitleInput.value;
     let author = bookAuthorInput.value;
     let pages = bookPagesInput.value;
-    myLibrary.push(new Book(title, author, pages, false));
+    let read = document.querySelector('input[name="book-read-input"]:checked').value == "true";
+
+    myLibrary.push(new Book(title, author, pages, read));
+    updateLocalStorage();
     populateLibrary();
 }
 
@@ -37,17 +51,33 @@ function addBookHTML(book, index) {
     newBookHTML.querySelector('.book-title').textContent = book.title;
     newBookHTML.querySelector('.book-author').textContent = book.author;
     newBookHTML.querySelector('.book-pages').textContent = book.pages;
+    newBookHTML.querySelector('.book-read').textContent = book.read ? 'Yes' : 'No';
+    newBookHTML.querySelector('.have-read').textContent = book.read ? 'Haven\'t read' : 'Have read';
+    newBookHTML.querySelector('.book').setAttribute('data-book-id', index);
+    newBookHTML.querySelector('.have-read').addEventListener('click', () => toggleBookReadStatus(index));
+    newBookHTML.querySelector('.delete-book').addEventListener('click', () => removeBookFromLibrary(index));
+
 
     existingBooks.appendChild(newBookHTML);
 }
 
-bookForm.addEventListener('submit', submitForm);
 
-function removeBookFromLibrary(book) {}
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    updateLocalStorage();
+    populateLibrary();
+}
 
-function toggleBookReadStatus(book) {}
+function toggleBookReadStatus(index) {
+    myLibrary[index].read = !myLibrary[index].read;
+    updateLocalStorage();
+    populateLibrary();
+}
 
 function populateLibrary() {
     existingBooks.innerHTML = '';
     myLibrary.forEach(addBookHTML);
 }
+
+
+populateLibrary();
