b8254a018d92ba04d2f5c30eb6ffa26c52ed4f40
eriksol <erik.solg@tallink.ee>
2020-10-28 07:52:16 +0200

    Convert library to modular JS. Use factory functions

--- library/script.js
+++ library/script.js
@@ -1,83 +1,102 @@
-let myLibrary = [];
-if (localStorage.getItem('myLibrary')) {
-    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
-}
+(function() {
+
+    let library = {
+
+        myLibrary: [],
+
+        init: function() {
+            this.getLocalStorage();
+            this.cacheDom();
+            this.bindEvents();
+            this.populateLibrary();
+        },
+
+        cacheDom: function() {
+            this.submitBook = document.querySelector('#submit-book');
+            this.bookTitleInput = document.querySelector('#book-title-input');
+            this.bookAuthorInput = document.querySelector('#book-author-input');
+            this.bookPagesInput = document.querySelector('#book-pages-input');
+            this.existingBooks = document.querySelector('#existing-books');
+            this.bookTemplate = document.querySelector('#book-template');
+            this.template = document.querySelector('#book-template');
+
+        },
+
+        bindEvents: function() {
+            this.submitBook.addEventListener('click', this.submitForm.bind(this));
+
+        },
+
+        updateLocalStorage: function() {
+            localStorage.setItem('myLibrary', JSON.stringify(this.myLibrary));
+        },
 
 
-const submitBook = document.querySelector('#submit-book');
-const bookTitleInput = document.querySelector('#book-title-input');
-const bookAuthorInput = document.querySelector('#book-author-input');
-const bookPagesInput = document.querySelector('#book-pages-input');
-const existingBooks = document.querySelector('#existing-books');
-const bookTemplate = document.querySelector('#book-template');
+        getLocalStorage: function() {
+            if (localStorage.getItem('myLibrary')) {
+                this.myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+            }
+        },
 
-submitBook.addEventListener('click', submitForm);
+        submitForm: function(event) {
+            event.preventDefault();
+            let title = this.bookTitleInput.value;
+            let author = this.bookAuthorInput.value;
+            let pages = this.bookPagesInput.value;
+            let read = document.querySelector('input[name="book-read-input"]:checked').value == "true";
 
-function updateLocalStorage() {
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
+            this.myLibrary.push(bookFactory(title, author, pages, read));
+            this.updateLocalStorage();
+            this.populateLibrary();
+        },
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read ? "have read" : "not read yet"}`;
+        getBookTemplate: function() {
+            return document.importNode(this.template.content, true);
+        },
+
+        addBookHTML: function(book, index) {
+            let newBookHTML = this.bookTemplate.content.cloneNode(true);
+
+            newBookHTML.querySelector('.book-number').textContent = `Book ${index + 1}`;
+            newBookHTML.querySelector('.book-title').textContent = book.title;
+            newBookHTML.querySelector('.book-author').textContent = book.author;
+            newBookHTML.querySelector('.book-pages').textContent = book.pages;
+            newBookHTML.querySelector('.book-read').textContent = book.read ? 'Yes' : 'No';
+            newBookHTML.querySelector('.have-read').textContent = book.read ? 'Haven\'t read' : 'Have read';
+            newBookHTML.querySelector('.book').setAttribute('data-book-id', index);
+            newBookHTML.querySelector('.have-read').addEventListener('click', () => this.toggleBookReadStatus(index));
+            newBookHTML.querySelector('.delete-book').addEventListener('click', () => this.removeBookFromLibrary(index));
+
+
+            this.existingBooks.appendChild(newBookHTML);
+        },
+
+
+        removeBookFromLibrary: function(index) {
+            this.myLibrary.splice(index, 1);
+            this.updateLocalStorage();
+            this.populateLibrary();
+        },
+
+        toggleBookReadStatus: function(index) {
+            this.myLibrary[index].read = !this.myLibrary[index].read;
+            this.updateLocalStorage();
+            this.populateLibrary();
+        },
+
+        populateLibrary: function() {
+            this.existingBooks.innerHTML = '';
+            this.myLibrary.forEach(this.addBookHTML.bind(this));
+        }
+
+    };
+
+
+    let bookFactory = (title, author, pages, read) => {
+
+        return { title, author, pages, read }
     }
-}
 
-function submitForm(event) {
-    event.preventDefault();
-    let title = bookTitleInput.value;
-    let author = bookAuthorInput.value;
-    let pages = bookPagesInput.value;
-    let read = document.querySelector('input[name="book-read-input"]:checked').value == "true";
+    library.init();
 
-    myLibrary.push(new Book(title, author, pages, read));
-    updateLocalStorage();
-    populateLibrary();
-}
-
-function getBookTemplate() {
-    let template = document.querySelector('#book-template');
-    return document.importNode(template.content, true);
-}
-
-function addBookHTML(book, index) {
-    let newBookHTML = bookTemplate.content.cloneNode(true);
-
-    newBookHTML.querySelector('.book-number').textContent = `Book ${index + 1}`;
-    newBookHTML.querySelector('.book-title').textContent = book.title;
-    newBookHTML.querySelector('.book-author').textContent = book.author;
-    newBookHTML.querySelector('.book-pages').textContent = book.pages;
-    newBookHTML.querySelector('.book-read').textContent = book.read ? 'Yes' : 'No';
-    newBookHTML.querySelector('.have-read').textContent = book.read ? 'Haven\'t read' : 'Have read';
-    newBookHTML.querySelector('.book').setAttribute('data-book-id', index);
-    newBookHTML.querySelector('.have-read').addEventListener('click', () => toggleBookReadStatus(index));
-    newBookHTML.querySelector('.delete-book').addEventListener('click', () => removeBookFromLibrary(index));
-
-
-    existingBooks.appendChild(newBookHTML);
-}
-
-
-function removeBookFromLibrary(index) {
-    myLibrary.splice(index, 1);
-    updateLocalStorage();
-    populateLibrary();
-}
-
-function toggleBookReadStatus(index) {
-    myLibrary[index].read = !myLibrary[index].read;
-    updateLocalStorage();
-    populateLibrary();
-}
-
-function populateLibrary() {
-    existingBooks.innerHTML = '';
-    myLibrary.forEach(addBookHTML);
-}
-
-
-populateLibrary();
+})();
