174352811c965722f1ff504ebe92a2143ad5ca8e
eriksol <erik.solg@tallink.ee>
2020-11-04 08:40:46 +0200

    Restructure modules. Make HTML

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,63 +1,127 @@
 let gameController = (function() {
-    let players = [];
-    let currentPlayer = null;
 
-    function determineCurrentOutcome() {
-
-    }
-
-    function switchPlayer(player) {
-
-    }
-
+    /**
+     * Disable gameBoard HTML. Display an outcome on the screen.
+     */
     function finishGame(outcome) {
 
     }
 
     function resetGame() {
         gameBoard.cleanBoard();
-    }
-
-    function createPlayers() {
-
+        playerController.resetPlayers();
     }
 
     function newTurn() {
         let currentOutcome = determineCurrentOutcome();
-        if (outcome) {
+        if (currentOutcome) {
             finishGame(outcome);
         }
-        switchPlayer();
+        playerController.switchPlayer();
     }
 
-    function newGame() {
+    function start() {
         resetGame();
-        createPlayers();
+        playerController.createPlayers();
         newTurn();
     }
 
+    /**
+     * Check, if the square is already marked. If not, then mark the square according to current player's symbol.
+     * Then call a newTurn().
+     */
     function handleSquareMarked(event) {
-        gameBoard.markSquare(currentPlayer, coordinate, content)
+        gameBoard.markSquare(coordinate)
         newTurn();
     }
 
-    newGame();
+    resetGame();
 
 })();
 
-let gameBoard = (function() {
-    let boardContent = [];
+let playerController = (function() {
+    let players;
+    let currentPlayer;
 
-    function cleanBoard() {
+    /**
+     * Resets HTML of player inputs
+     */
+    function resetPlayerBoxes() {
 
     }
 
+    /**
+     * Changes HTML to remove player inputs, display only the names.
+     */
+    function makePlayerBoxesGameStart() {
+
+    }
+
+    /**
+     * If no currentPlayer exists. Take first player
+     * If exists, take other than currentPlayer.
+     * Makes currentPlayer player box active.
+     */
+    function switchPlayer() {
+
+    }
+
+    /**
+     * Will create two players. First player will be assigned symbol 'X', second one 'O'.
+     * Will put the players into the players array.
+     */
+    function createPlayers() {
+
+
+    }
+
+    function resetPlayers() {
+        players = [];
+        currentPlayer = null;
+    }
+
+    return {
+        switchPlayer,
+        currentPlayer
+    }
+})
+
+let gameBoard = (function() {
+    let boardContent;
+    const UNASSIGNED = 0;
+    const X = 1;
+    const Y = 2;
+    const TIE = 3;
+
+    function cleanBoard() {
+        boardContent = [
+            UNASSIGNED, UNASSIGNED, UNASSIGNED,
+            UNASSIGNED, UNASSIGNED, UNASSIGNED,
+            UNASSIGNED, UNASSIGNED, UNASSIGNED
+        ]
+        drawBoard();
+    }
+
+    /** 
+     * Will take the current boardContent and make changes in HTML
+     */
     function drawBoard() {
 
     }
 
-    function markSquare(player, coordinate, content) {
+    /** 
+     * Will put corresponding symbol to the boardContent position
+     */
+    function markSquare(coordinate) {
         drawBoard();
+    }
+
+    /**
+     * Check boardContent and determine, if anyone has won the game,
+     * or is it a tie. Returns X, Y, TIE or null.
+     */
+    function determineCurrentOutcome() {
+
     }
 
     return {
@@ -68,11 +132,7 @@ let gameBoard = (function() {
 });
 
 
-let playerFactory = (name, isComputer, color) => {
+let playerFactory = (name, symbol) => {
 
-    return { name, isComputer, color };
-}
-
-let squareFactory = (player, content) => {
-    return { player, content };
+    return { name, symbol };
 }
