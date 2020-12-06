e1f6c587b5ede928856626310bd98bc9e4943674
eriksol <erik.solg@tallink.ee>
2020-12-06 12:06:19 +0200

    changes

--- weather-app/src/assets/addParkedVan.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `RESERVATION_VEHICLE_CATEGORY`(`id`, `car_height`, `car_length`, `code`, `car_width`,
-                                           `car_weight`)
-VALUES (300000001798105, 2.4, 7.0, 'PARKED VAN', 2.50, NULL);
